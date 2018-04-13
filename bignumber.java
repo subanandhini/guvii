4b07e5939d9b636c79a5834a282e9ab205d573b8
subanandhini <34816663+subanandhini@users.noreply.github.com>
2018-04-13 18:47:23 +0530

    Create bignumber.java

---
+++ bignumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Duplicate {
+	public static void main(String[] args) {
+		int n=0;
+	System.out.println("enter total number ");
+	
+Scanner sc= new Scanner(System.in);
+n=sc.nextInt();
+System.out.println("enter number");
+	int a[]=new int[n];
+	for(int i=0;i<n;i++) {
+		a[i]=sc.nextInt();}
+	for(int i=0;i<n;i++) {
+		int max=0;
+		 	if(a[i]>max) 
+		 	{
+		 		max=a[i];
+			}
+				System.out.print(max);
+			}
+	}
+	}
