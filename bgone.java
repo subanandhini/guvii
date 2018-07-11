01fe19f383605699cb63f3c7fc8f79f59d3f4b19
subanandhini <34816663+subanandhini@users.noreply.github.com>
2018-07-11 10:41:15 +0530

    Create bgone.java

---
+++ bgone.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class bgone{
+public static void main(String []){
+Scanner s= new Scanner(System.in);
+int i=s.nextInt();
+if(i!=0)
+System.out.println("Positive");
+else
+System.out.println("zero");}}
