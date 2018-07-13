f77c8016f25eeb74d137c85753c39b3bedb54d6a
Nandhini k <subanandhini08@gmail.com>
2018-07-13 23:22:44 +0530

    Create constant.java

---
+++ constant.java
@@ -0,0 +1,12 @@
+import java.util.*
+class constant{
+public static void main(String [] agrs){
+Scanner s=new Scanner(System.in);
+char c=s.next().charAt(0);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+System.out.println("vowels");
+else 
+System.out.println("Constant");
+
+
+}}
