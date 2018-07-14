8c9a6bacadb79833086cc5d25524f3269329225f
Nandhini k <subanandhini08@gmail.com>
2018-07-14 16:06:53 +0530

    Update pro_2.java

--- pro_2.java
+++ pro_2.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -6,19 +7,23 @@ public class Pro_2 {
 public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
 	System.out.println("enter number");
-	long num=s.nextLong();
+	BigInteger num =s.nextBigInteger();
 	System.out.println("enter no of digits to be removed");
 	byte b=s.nextByte();
-	String anum=Long.toString(num);
+	String anum=""+num;
 	int has=0;
-	ArrayList<Integer> aa=new ArrayList<Integer>();
+	ArrayList<String> aa=new ArrayList<String>();
+	if(b!=0)
 	for(int i=0;i<anum.length()-b;i++) {
 		has=i;
 	    for(int j=0;j<anum.length()-b;j+=b) {
-	    	aa.add(Integer.parseInt(anum.substring(has,has+b)));
+	    	aa.add(anum.substring(has,has+b));
 	    	
 	}
 	}
+	else
+		aa.add(anum);
+	
 Collections.sort(aa);
 	System.out.println(aa.get(0));
 }
