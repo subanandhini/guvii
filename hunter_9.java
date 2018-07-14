96d43f8b8ca86d3dca5282b318e7d72f3e545a40
Nandhini k <subanandhini08@gmail.com>
2018-07-15 00:41:25 +0530

    Create hunter_9.java

---
+++ hunter_9.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class hunter_9 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n1=sc.nextInt();
+	int n2=sc.nextInt();
+	String pm="";int c=0;
+	for(int n=n1;n<=n2;n++){     
+		int f=0;
+		for(int i=n;i>=1;i--){      
+		    if(n%i==0){       
+		     f=f+1;      
+		    }      
+	            }
+		if(f==2) {
+			pm=pm+n+" ";
+			c++;
+		}
+			
+}System.out.println(c);
+}}
