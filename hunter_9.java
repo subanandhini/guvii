2bd7665b70f757f7d07639370cb6e1d56aa385e3
Nandhini k <subanandhini08@gmail.com>
2018-07-15 00:42:36 +0530

    Update hunter_9.java

--- hunter_9.java
+++ hunter_9.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class hunter_9 {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter two numbers");
 	int n1=sc.nextInt();
 	int n2=sc.nextInt();
 	String pm="";int c=0;
