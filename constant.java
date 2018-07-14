f9709f72d72d9e6310182d2806717b304d151797
Nandhini k <subanandhini08@gmail.com>
2018-07-14 09:29:41 +0530

    Update constant.java

--- constant.java
+++ constant.java
@@ -2,11 +2,13 @@ import java.util.*
 class constant{
 public static void main(String [] agrs){
 Scanner s=new Scanner(System.in);
-char c=s.next().charAt(0);
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-System.out.println("vowels");
-else 
-System.out.println("Constant");
+char a=s.next().charAt(0);
+if((a>=65&&a<=90&&a!='A'&&a!='e'&&a!='i'&&a!='o'&&a!='u')||(a>=97&&a<=122&&a!='A'&&a!='E'&&a!='I'&&a!='O'&&a!='U'))
+		System.out.println("consonant");
+	else if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+		System.out.println("Vowel");
+	else
+		System.out.println("InValid");
 
 
 }}
