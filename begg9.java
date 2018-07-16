9ab6305fc4d930689d735551c610f9750621bf58
Nandhini k <subanandhini08@gmail.com>
2018-07-16 15:40:11 +0530

    Create begg9.java

---
+++ begg9.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class begg9 {
+
+	public static void main(String[] args) {
+    Scanner s=new Scanner (System.in);
+    int n=s.nextInt();
+    int sum=0;
+   for(int i=0;i<n;i++) 
+	   sum+=i;
+   
+	System.out.println(sum);}
+}
