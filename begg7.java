ccd8ac5463f984315dc3dc46ee1c9b4655612ec3
Nandhini k <subanandhini08@gmail.com>
2018-07-16 15:36:20 +0530

    Create begg7.java

---
+++ begg7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class begg7 {
+
+	public static void main(String[] args) {
+    Scanner s=new Scanner (System.in);
+    int n=s.nextInt();
+   for(int i=0;i<n;i++) 
+	   System.out.println("Hello");
+   }
+
+}
