77ebd15088efdd77e84913347aaac2b62640edbe
Nandhini k <subanandhini08@gmail.com>
2018-07-16 15:32:59 +0530

    Create begg6.java

---
+++ begg6.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class begg6 {
+
+	public static void main(String[] args) {
+    Scanner s=new Scanner (System.in);
+  int a=s.nextInt();
+System.out.println((a%4==0)?"yes":"NO");
+	}
+}
