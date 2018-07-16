d803159fa8330278e378a1222f6c6f73e9ea41bc
Nandhini k <subanandhini08@gmail.com>
2018-07-16 16:35:48 +0530

    Create begg10.java

---
+++ begg10.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class begg10 {
+
+	public static void main(String[] args) {
+    Scanner s=new Scanner (System.in);
+    int n=s.nextInt();
+   String as=Integer.toString(n);
+   System.out.println(as.length());
+	}
+}
