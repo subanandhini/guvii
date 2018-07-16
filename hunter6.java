4c5b28758056372a5ea474a3a8ec1a514e3b283c
Nandhini k <subanandhini08@gmail.com>
2018-07-16 17:59:21 +0530

    Update hunter6.java

--- hunter6.java
+++ hunter6.java
@@ -4,16 +4,19 @@ public static void main(String[] args) {
 	 Scanner s=new Scanner(System.in);
      int a=s.nextInt();
 	int aa[]=new int[a];
+	int co=1;
 	for(int i=0;i<a;i++) {
 		aa[i]=s.nextInt();
 	}
-	in:for(int i=0;i<a;i++) {
+	in:for(int i=0;i<a-1;i++) {
 		if(aa[i]==aa[i+1]) {
 			System.out.println(aa[i]);
+			co++;
 			break in;
-		}
-		
-	}
+		}}
+	if(co==1)
+			System.out.println("Unique");
+	
 	
 	s.close();
 }
