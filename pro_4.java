22b6068aa1b627a802fc36cb0d98a8a442e30f70
Nandhini k <subanandhini08@gmail.com>
2018-08-06 18:00:08 +0530

    Create pro_4.java

---
+++ pro_4.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class pro_4  {
+public static void main(String[] args) {
+	//System.out.println("fgfyd");
+		Scanner ss=new Scanner(System.in);
+		System.out.println("Enter two values");
+	    String s=ss.next();
+	    String a=ss.next();
+	    boolean c;
+	   int counter=0;
+	    int len=s.length();
+	    		if(s.length()<a.length())
+	    			{
+	    			len=a.length()-s.length();
+	    			c=true;}
+	    		else {
+	    			len=s.length()-a.length();
+	    			c=false;
+	    			}
+	   if(c==true) {
+	    for(int i=0;i<s.length();i++) {
+	    	if(a.charAt(i)==s.charAt(i))
+	    		continue;
+	    	else
+	    		counter+=Math.abs(a.charAt(i)-s.charAt(i));
+	    	
+	    }}
+	   if(c!=true) {
+		    for(int i=0;i<a.length();i++) {
+		    	if(a.charAt(i)==s.charAt(i))
+		    		continue;
+		    	else
+		    		counter+=Math.abs(a.charAt(i)-s.charAt(i));
+		    	
+		    }}
+	    
+	System.out.println(counter+len);}
+
+}
